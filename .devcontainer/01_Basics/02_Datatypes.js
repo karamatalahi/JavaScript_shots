12ed13a7c983242cb66747f17f260365df61d229
karamatalahi <60042240+karamatalahi@users.noreply.github.com>
2023-09-30 06:15:24 +0000

    Datatypes

---
+++ .devcontainer/01_Basics/02_Datatypes.js
@@ -0,0 +1,17 @@
+"use strict";  //Treat all code as new version of js will check everything there will be no ignorance
+
+// alert("hello i am working");  will not work we are using node js not browser
+
+// types of DataTypes
+/*
+PRIMITIVE DATATYPES:
+Numbers
+Strings 
+Boolean
+BigInt
+Null  //Stand Alone value
+Undefined  //it is when the Value is not defined to a variable
+Symbol
+Object
+
+*/ 
