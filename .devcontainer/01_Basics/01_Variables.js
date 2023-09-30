1d3b3e159fbe3b53e1737b9cb41853bfaf77bfec
karamatalahi <60042240+karamatalahi@users.noreply.github.com>
2023-09-30 06:17:55 +0000

    Datatypes Done

---
+++ .devcontainer/01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 1235;
+let accountEmail = 'karamat@google.com'
+var accountPassword = 1235;
+accountCity = 'jammu'
+let accountState ;
+// accountId = 4567;   not Allowed in Case of Constant
+// console.log(accountId);
+/*Don't use var ever because of issue in block scope and functional scope*/
+
+
+accountEmail = 'umar@mail.com'
+accountPassword= 459495;
+accountCity= 'Mohali'
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
