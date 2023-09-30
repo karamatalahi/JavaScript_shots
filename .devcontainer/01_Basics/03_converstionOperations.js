c6b69d1877d3d1cb744329549f49e879c2816ef1
karamatalahi <60042240+karamatalahi@users.noreply.github.com>
2023-09-30 06:35:25 +0000

    Converstion of DataTypes

---
+++ .devcontainer/01_Basics/03_converstionOperations.js
@@ -0,0 +1,9 @@
+let score = "33";
+console.log(typeof(score))  //this will show the type of score is string
+
+let numberValue = Number(score)  //this will convert to number 
+console.log(typeof(numberValue))  //it will show the type will be number doesn't matte what we eneter
+
+//when we convert 33abc which give NaN 
+//and the type will be number
+//true is converted to 1 and false to 0
