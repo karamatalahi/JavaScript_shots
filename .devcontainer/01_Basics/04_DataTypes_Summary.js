e1cc08099f2f3af75b4e240247059f26256bacf0
karamatalahi <60042240+karamatalahi@users.noreply.github.com>
2023-10-02 05:46:01 +0000

    DataType Summary

---
+++ .devcontainer/01_Basics/04_DataTypes_Summary.js
@@ -0,0 +1,25 @@
+/* # primitive Data-types
+7 types:
+String
+Boolean 
+Number
+Null
+Undefined
+Symbol
+BigInt
+
+
+JavaScript is a Dynamically typed Language:
+
+#Refrence Type or Non-Primitive Data-types  
+ Arrays
+ Objects
+ Functions
+  
+  */
+
+//  some Examples of symbol
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id===anotherId);
