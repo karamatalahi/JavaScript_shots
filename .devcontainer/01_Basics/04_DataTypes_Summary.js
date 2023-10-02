0e60f2100367d8cb3837b25b867b488e37e78b26
karamatalahi <60042240+karamatalahi@users.noreply.github.com>
2023-10-02 06:01:24 +0000

    Memory Allocation Stack and Heap

--- .devcontainer/01_Basics/04_DataTypes_Summary.js
+++ .devcontainer/01_Basics/04_DataTypes_Summary.js
@@ -23,3 +23,33 @@ JavaScript is a Dynamically typed Language:
 const id = Symbol('123');
 const anotherId = Symbol('123');
 console.log(id===anotherId);
+
+
+
+//++++++++++++++++++++++++ Memories and It's types +++++++++++++++++++++++++++++
+
+//Basically memory is of two types 
+//Stack Memory:--Used in Primitive DataTypes
+//Heap Memory:--:Used in Non-Primitve DataTypes
+
+//Stack Example
+let myName = 'Karamat ely'
+let mySecondName = myName;
+// console.log(mySecondName);
+// mySecondName= 'king'
+// console.log(myName)
+// console.log(mySecondName);
+
+//in stack memory it actually throws a copy of the original so if we make changes in copy it will not reflect in the original 
+
+// Heap Example 
+
+let user = {
+    name:'karmat',
+    email:'karamat@ybl.com'
+}
+let user2= user;
+user2.email= 'karamat@google.com'
+console.log(user.email)
+
+//so this reflects that if we make any changes in user two that will automatically reflect to user1 also because they both are pointing to same memory in the heap 
