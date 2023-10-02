fbee6a290b810bef90993b9f67a2f2d5d933e2a8
karamatalahi <60042240+karamatalahi@users.noreply.github.com>
2023-10-02 07:11:11 +0000

    Strings all Mehtods

---
+++ .devcontainer/01_Basics/05_Strings.js
@@ -0,0 +1,23 @@
+// Strings 
+// Main points only 
+// const gameName = new String("king")
+// console.log(gameName)
+// const name = 'Guardian'
+// // if we want to find a character 
+// console.log(name.charAt(4))
+// console.log(name.indexOf('a'));
+
+// const newGame = 'Battleground'
+// const substr = newGame.substring(0,4);  //this will give batt till 0,3
+// console.log(substr);
+
+// const anotherName = 'mukeshCha'
+// const anotherSlice = anotherName.slice(-8,3);
+// console.log(anotherSlice)
+
+//trim removes the extra spaces in a string
+
+
+//in any url if we want to replace something like a user enters space and we want to convert that to % we will do something like url.replace(' ','%')  and url.include('king') will check does king exist in this url
+
+
